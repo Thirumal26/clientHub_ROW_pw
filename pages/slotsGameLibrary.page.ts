5b9b872c9ec96cdb8b792d710e9ad84c75c61966
Regallagadda Thirumal <thirumal.r@techmojo.in>
2026-02-06 10:50:43 +0530

    latest_code

--- pages/slotsGameLibrary.page.ts
+++ pages/slotsGameLibrary.page.ts
@@ -1,4 +1,5 @@
 import { Page, expect, Locator } from "@playwright/test";
+import { count } from "console";
 import { request } from "http";
 export class slotsGameLibrary{
     readonly page: Page
@@ -6,15 +7,24 @@ export class slotsGameLibrary{
     readonly gamelibrarySB: Locator
     readonly sortDropDown: Locator
     readonly sortA_Zbutton: Locator
+    readonly certifiedCountryDropDown: Locator
+    readonly exclusivityDropDown: Locator
+    readonly categoryDropDown: Locator
 
     constructor(page:Page){
         this.page = page
         this.slotsSidebar = this.page.locator('.sidebar-menu__item--icon-casino')
         this.gamelibrarySB = this.page.getByRole('link',{name: 'Game Library'})
-        this.sortDropDown = this.page.locator('[data-game-library-filter="sort"]').locator('..')
-        .locator('.selectize-input')
+        this.sortDropDown = this.page.locator('[data-game-library-filter="sort"]')
+        .locator('..').locator('.selectize-input')
         this.sortA_Zbutton = this.page.locator('[data-game-library-filter="sort"]').locator('..')
-        // .locator('selectize-dropdown-content')
+        this.certifiedCountryDropDown = this.page.locator('[data-game-library-filter="certified-country"]')
+        .locator('..').locator('.selectize-input')
+        this.exclusivityDropDown = this.page.locator('[data-game-library-filter="game-target-market"]')
+        .locator('..').locator('selectize-input')
+        this.categoryDropDown = this.page.locator('[data-game-library-filter="game-new-studio-filter"]')
+        .locator('..').locator('selectize-input')
+
     }
     async NavigateSlotsGameLibrary(){
         expect(this.slotsSidebar).toBeTruthy()
@@ -27,13 +37,37 @@ export class slotsGameLibrary{
         expect(apiResponse.status()).toBe(200)
         await expect(this.page).toHaveURL(/\/slots\/game-library\/?/)
     }
-    async validateGBFilters(){
+    async validateGBFiltersSlots(){
         await this.sortDropDown.click()
         const sortA_ZbuttonClick = this.sortA_Zbutton.locator('.selectize-dropdown .option', { hasText: 'A-Z' })
         await sortA_ZbuttonClick.click()
         const sortFilter = this.page.locator('[data-game-library-filter="sort"]').locator('..');
         await expect(sortFilter.locator('.selectize-input .item')).toHaveText('A-Z');
     }
-    
+    async validateGBFiltersCertifiedCountry(){
+        await this.certifiedCountryDropDown.click()
+        const options = this.page.locator('.selectize-dropdown-content .option:not(.disabled)')
+        const count = await options.count();
+        const randomIndex = Math.floor(Math.random() * count)
+        await options.nth(randomIndex).click()
+    }
+    async validateGBFiltersExclusivity(){
+        await this.exclusivityDropDown.click()
+        const options = this.page.locator('.selectize-dropdown-content .option:not(.disabled)')
+        const count = await options.count();
+        const randomIndex = Math.floor(Math.random() * count)
+        await options.nth(randomIndex).click()
+    }
+    async validateGBFiltersCategory(){
+        await this.categoryDropDown.click()
+        const options = this.page.locator('.selectize-dropdown-content .option:not(.disabled)')
+        const count = await options.count();
+        const randomIndex = Math.floor(Math.random() * count)
+        await options.nth(randomIndex).click()
+        const clear = this.page.locator('.clear')
+        clear.click()
+    }
+
+
 
 }
